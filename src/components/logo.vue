43a2ec8b6b2edbc76d74f85d5cf0772834edf47d
Guilherme Scaranse <guiscaranse@gmail.com>
2019-09-16 13:33:08 -0300

    Fixes svg inliner

--- src/components/logo.vue
+++ src/components/logo.vue
@@ -1,14 +1,10 @@
 <template>
-  <inliner src="/static/teste.svg"></inliner>
+  <img svg-inline src="@/assets/guilherme.svg">
 </template>
 
 <script>
-import inliner from "./inliner.vue";
 export default {
   name: "logo",
-  components: {
-    inliner
-  },
   mounted() {
     const targets = this.$el.querySelectorAll(".path");
     this.$anime({
